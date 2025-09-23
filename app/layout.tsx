faf6fa7731a1279bc45f254d25a499fd8ef62cef
Junho Yoon <enffl95@icloud.com>
2025-09-23 22:27:01 +0900

    Link 와 a 의 차이

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/layouts/Header";
+import Footer from "@/components/layouts/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,12 +25,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+      <html lang="en">
+          <body
+              className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          >
+              <Header />
+              {children}
+              <Footer />
+          </body>
+      </html>
   );
 }
